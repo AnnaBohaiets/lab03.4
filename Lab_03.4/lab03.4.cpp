e74c0818846a99ade4a60d4206abc080e5477e29
AnnaBohaiets <grynko.n@gmail.com>
2023-10-05 12:28:08 +0300

    first commit

---
+++ Lab_03.4/lab03.4.cpp
@@ -0,0 +1,26 @@
+// Lab_03_4.cpp
+// Богаєць Анна
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 3
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	double R; // вхідний аргумент
+	double x; // вхідний аргумент
+	double y; // вхідний параметр
+	cout << "R = "; cin >> R;
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	
+
+	// розгалуження в повній формі
+	if((y <= R && y >= 0 && x >= -R && x <= 0 && R * R <= (x + R) * (x + R) + (y - R) * (y - R)) ||	(y >= -R && y <= 0 && x >= 0 && x <= R && R * R <= (x - R) * (x - R) + (y + R) * (y + R)) ||(x>=0 && x<= R&& y<=R&& y>=0) || (x >= -R && x <= 0 && y <=0 && y >= -R))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	cin.get();
+	return 0;
+}
